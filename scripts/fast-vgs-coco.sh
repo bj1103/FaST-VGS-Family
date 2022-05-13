9d4292dd8843de63ca752e72d44372920590174b
Shampoowang <jeff890322@gmail.com>
2022-05-13 16:28:04 +0800

    Use TWCC setting

--- scripts/fast-vgs-coco.sh
+++ scripts/fast-vgs-coco.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate my_env
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate my_env
 export CUDA_VISIBLE_DEVICES=0
 
-data_root=$1
-raw_audio_base_path=$2
-fb_w2v2_weights_fn=$3
-exp_dir=$4
+data_root=/work/vjsalt22/poheng/coco
+raw_audio_base_path=/work/vjsalt22/dataset/coco/SpokenCOCO
+fb_w2v2_weights_fn=/work/vjsalt22/poheng/fb_w2v2/wav2vec_small.pt
+exp_dir=/work/vjsalt22/poheng/exp_coco
 
 python \
 ../run_spokencoco.py \
@@ -16,18 +16,18 @@ python \
 --exp_dir ${exp_dir} \
 --num_workers 4 \
 --batch_size 64 \
---val_batch_size 100 \
---val_cross_batch_size 8000 \
+--val_batch_size 64 \
+--val_cross_batch_size 200 \
 --n_epochs 20 \
---n_print_steps 800 \
---n_val_steps 4000 \
---lr 0.0001 \
+--n_print_steps 200 \
+--n_val_steps 6400 \
+--lr 0.05 \
 --warmup_fraction 0.1 \
 --xtrm_layers 2 \
 --trm_layers 6 \
---fine_matching_weight 1.0 \
---coarse_matching_weight 0.1 \
+--fine_matching_weight 0 \
+--coarse_matching_weight 1 \
 --coarse_to_fine_retrieve \
 --feature_grad_mult 0. \
---layer_use 7
-
+--layer_use 7 \
+--solo_loss VICReg
--- scripts/fast-vgs-flickr8k.sh
+++ scripts/fast-vgs-flickr8k.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate my_env
-export CUDA_VISIBLE_DEVICES=2,3
+#source ~/miniconda3/etc/profile.d/conda.sh
+#conda activate my_env
+export CUDA_VISIBLE_DEVICES=0
 
-data_root=$1
-raw_audio_base_path=$2
-fb_w2v2_weights_fn=$3
-exp_dir=$4
+data_root=/work/vjsalt22/poheng/flickr
+raw_audio_base_path=/work/vjsalt22/dataset/flickr/flickr_audio
+fb_w2v2_weights_fn=/work/vjsalt22/poheng/fb_w2v2/wav2vec_small.pt
+exp_dir=/work/vjsalt22/poheng/exp_2_flickr
 
 python \
 ../run_flickr8k.py \
@@ -15,18 +15,19 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --num_workers 4 \
---batch_size 96 \
---val_batch_size 100 \
---val_cross_batch_size 8000 \
---n_epochs 20 \
+--batch_size 64 \
+--val_batch_size 64 \
+--val_cross_batch_size 128 \
+--n_epochs 250 \
 --n_print_steps 312 \
---n_val_steps 312 \
+--n_val_steps 624 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --xtrm_layers 2 \
 --trm_layers 6 \
---fine_matching_weight 1.0 \
---coarse_matching_weight 0.1 \
+--fine_matching_weight 0 \
+--coarse_matching_weight 1.0 \
 --coarse_to_fine_retrieve \
 --feature_grad_mult 0. \
---layer_use 7
+--layer_use 7 \
+--validate
